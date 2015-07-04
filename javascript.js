131985df590a76aa9d90e9e1c4098eb936b410c9
Mladen-Karadimov <mladen.karadimov@gmail.com>
2015-07-04 03:22:03 +0300

    Adding better vision, animations, menu

--- javascript.js
+++ javascript.js
@@ -2,10 +2,32 @@ var main = function() {
 	$('#go_on').click(function() {
 		$('#welcome_window').animate({
 			left: "-105%"
-		}, 3000);
+		}, 250);
 
-		$('body').delay(30000).css("background-color","#9D9D9D");
+		$('#top_menu').animate({
+			top: "0"
+		}, 500);	
+
+		$('body').delay(30000).css("background-color","#B6B6B6");
+
+		$('#top_menu #menu_container ul li').delay(350).animate({
+			fontSize: '45px'
+		}, 250);
+		
 	});
+
+	$('#welcome_window h2').hover(function() {
+		$(this).animate({
+			fontSize : '84px'
+		}, 300);
+	},
+
+	function() {
+		$(this).animate({
+			fontSize : '72px'
+		}, 250);
+	});	
+
 }
 
 $(document).ready(main);
